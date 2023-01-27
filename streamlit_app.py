bb302c8fd23827e18dd573735ec1c21019eb66f5
shivbholesingh <34954928+shivbholesingh@users.noreply.github.com>
2023-01-27 12:15:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,4 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title("Love")
-streamlit.header('I Love You ananya singh')
-streamlit.text('anu you are my sunshine')
-streamlit.text('Diksha is very pretty')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
