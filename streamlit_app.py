78308c5318640426566d9e7d2b182da185760aa3
shivbholesingh <34954928+shivbholesingh@users.noreply.github.com>
2023-01-30 13:23:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,8 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
+# put the dafta into a dataframe 
+df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
